2004fd02e5bf2dc9d6cebc720097023d887a8641
Muhammad JUNAID <junaidwattoo33@gmail.com>
2024-10-20 02:29:13 -0700

    Commited

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,15 +1,15 @@
-// import Map from "../components/Map";
-// import Sidebar from "../components/Sidebar";
-// import User from "../components/User";
+
+import Sidebar from "../components/Sidebar";
+import Map from "../components/Map";
 
 import styles from "./AppLayout.module.css";
 
 function AppLayout() {
   return (
     <div className={styles.app}>
-      {/* <Sidebar />
-      <Map />
-      <User /> */}
+      <Sidebar />
+    <Map/>
+    
     </div>
   );
 }
